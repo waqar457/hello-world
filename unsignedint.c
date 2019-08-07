b80ba965105db135249deb660a959a740f875ee4
waqar457 <53616075+waqar457@users.noreply.github.com>
2019-08-07 10:13:53 +0530

    Add files via upload

---
+++ unsignedint.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    unsigned int distance;
+    printf("Enter the distance between tow planets:");
+    scanf("%u",&distance);
+    printf("you have entered:%u ",distance);
+    getch();
+}
