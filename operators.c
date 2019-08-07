c1818e0beba49f0e492cf1d80c177fd93ad90dc3
waqar457 <53616075+waqar457@users.noreply.github.com>
2019-08-07 10:54:28 +0530

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter first number=");
+    scanf("%d",&a);
+    printf("Enter second number=");
+    scanf("%d",&b);
+    c=a+b;
+    printf("sum= %d",c);
+    getch();
+}
